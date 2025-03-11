1a281b0681e35273e72dc33a7a816e723726f833
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-03-11 17:22:18 +0530

    Fix:scrollbar-hide

--- src/Components/SliderCards.jsx
+++ src/Components/SliderCards.jsx
@@ -3,7 +3,7 @@ import { movieData } from "../data/movieData";
 
 const SliderCards = () => {
   return (
-    <div className="max-h-[600px] flex justify-center items-start overflow-y-auto p-6">
+    <div className="max-h-[600px] flex justify-center items-start overflow-y-auto p-6 scrollbar-hide">
       {/* Cards Container */}
       <div className="grid grid-cols-2 md:grid-cols-1 gap-8 w-[450px] md:w-[250px]">
         {movieData.slice(0, 5).map((movie) => (
